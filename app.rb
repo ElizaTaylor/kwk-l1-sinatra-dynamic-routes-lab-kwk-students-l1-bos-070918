e992afd383a8746a7e268a2606addf7dd0059443
Eliza Taylor <elizataylor2002@gmail.com>
2018-07-18 13:32:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,27 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+gets '/say/:number/:phrase'
+end
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+  # Write your code here!
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    @reversed_string = @name.reverse
+    
+    "Your reversed string is #{@reversed_string}"
+  
+  end
+
+  get '/square/:number' do 
+  
+
+  
+  end
+
+
 
 end

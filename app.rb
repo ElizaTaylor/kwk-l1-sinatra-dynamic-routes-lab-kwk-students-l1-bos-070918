c80f72f38b2cf89539c6cdd38f06b8525e128bcb
Eliza Taylor <elizataylor2002@gmail.com>
2018-07-18 13:32:27 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,20 +9,4 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
-  get '/reversename/:name' do
-    @name = params[:name]
-    @reversed_string = @name.reverse
-    
-    "Your reversed string is #{@reversed_string}"
   
-  end
-
-  get '/square/:number' do 
-  
-
-  
-  end
-
-
-
-end
